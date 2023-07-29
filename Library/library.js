bedb385335549960dc53a4edb38e9592f73a6b66
Darina Usanova <darinushkaaaa@gmail.com>
2023-07-29 21:12:12 +0200

    feat: add main structure

---
+++ Library/library.js
@@ -0,0 +1,8 @@
+console.log(`
+1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md
+\n
+2. Deploy: (https://rolling-scopes-school.github.io/darinausanova-JSFEPRESCHOOL2023Q2/library/)
+\n
+3. Done 29.07.2023 / deadline 30.07.2023
+\n
+4. Score 100/100`);
