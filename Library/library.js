6fcae1d5d13fdc7ebac833b3dd586a7d2268177f
Darina Usanova <darinushkaaaa@gmail.com>
2023-08-12 15:25:46 +0200

    fix: change js

--- Library/library.js
+++ Library/library.js
@@ -1,11 +1,11 @@
-/*console.log(`
-1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md
+console.log(`
+1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part2.md
 \n
 2. Deploy: (https://rolling-scopes-school.github.io/darinausanova-JSFEPRESCHOOL2023Q2/Library/)
 \n
-3. Done 29.07.2023 / deadline 30.07.2023
+3. Done 12.08.2023 / deadline 14.08.2023
 \n
-4. Score 100/100`);*/
+4. Score 50/50`);
 
 //Burger handler
 (function () {
