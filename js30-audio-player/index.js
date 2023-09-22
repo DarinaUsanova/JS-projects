1eb1b0a92e9db88047a5551845184adddb2d2997
Darina Usanova <darinushkaaaa@gmail.com>
2023-09-22 17:58:36 +0300

    feat: update volume js

--- js30-audio-player/index.js
+++ js30-audio-player/index.js
@@ -1,6 +1,23 @@
-let progress = document.getElementById("progress")
-let song = document.getElementById("song")
-let ctrlIcon = document.getElementById("ctrlIcon")
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+const songs = [
+  {
+    title: "Beyonce",
+    name: "Lemonade",
+    source: "./assets/beyonce.mp3",
+    cover: "./assets/lemonade.png"
+  },
+  {
+    title: "Dua Lipa",
+    name: "Dont start now",
+    source: "./assets/dontstartnow.mp3",
+    cover: "./assets/dontstartnow.png"
+  }
+];
+
+let currentSongIndex = 0;
 
 song.onloadedmetadata = function(){
   progress.max = song.duration;
@@ -19,8 +36,44 @@ function playPause(){
     ctrlIcon.classList.remove("fa-play");
   }
 }
-if(song.play()){
-  setInterval(()=>{
-    progress.value = song.currentTime;
-  }, 500);
+
+function nextSong() {
+  currentSongIndex++;
+  if (currentSongIndex >= songs.length) {
+    currentSongIndex = 0;
+  }
+  updateSongInfo();
+  song.play();
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
 }
+
+function prevSong() {
+  currentSongIndex--;
+  if (currentSongIndex < 0) {
+    currentSongIndex = songs.length - 1;
+  }
+  updateSongInfo();
+  song.play();
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+}
+
+setInterval(() => {
+  progress.value = song.currentTime;
+}, 500);
+
+progress.onchange = function(){
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+}
+
+function updateSongInfo() {
+  song.src = songs[currentSongIndex].source;
+  document.querySelector(".cover").src = songs[currentSongIndex].cover;
+  document.querySelector(".title").textContent = songs[currentSongIndex].title;
+  document.querySelector(".name").textContent = songs[currentSongIndex].name;
+}
+updateSongInfo();
