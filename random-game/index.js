ab804e680702ab05ef549371ed6bb02cdbdfda73
Darina Usanova <darinushkaaaa@gmail.com>
2023-10-01 19:34:59 +0300

    fix: change path for img

--- random-game/index.js
+++ random-game/index.js
@@ -1,4 +1,4 @@
-//variables
+// variables
 const moves = document.getElementById("moves-count");
 const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
@@ -11,31 +11,32 @@ let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
+let firstCardValue;
 
-//Items array
+// Items array
 const items = [
-  {name: "firts", image: "first.jpeg"},
-  {name: "second", image: "second.jpeg"},
-  {name: "third", image: "third.jpeg"},
-  {name: "fourth", image: "fourth.jpeg"},
-  {name: "fifth", image: "fifth.jpeg"},
-  {name: "sixth", image: "sixth.jpeg"},
-  {name: "seventh", image: "seventh.jpeg"},
-  {name: "eight", image: "eigth.jpeg"},
-  {name: "ninth", image: "ninth.jpeg"},
-  {name: "tenth", image: "tenth.jpeg"},
-  {name: "eleventh", image: "elevens.jpeg"},
+  { name: "first", image: "img/first.jpeg" },
+  { name: "second", image: "img/second.jpeg" },
+  { name: "third", image: "img/third.jpeg" },
+  { name: "fourth", image: "img/fourth.jpeg" },
+  { name: "fifth", image: "img/fifth.jpeg" },
+  { name: "sixth", image: "img/sixth.jpeg" },
+  { name: "seventh", image: "img/seventh.jpeg" },
+  { name: "eight", image: "img/eighth.jpeg" },
+  { name: "ninth", image: "img/ninth.jpeg" },
+  { name: "tenth", image: "img/tenth.jpeg" },
+  { name: "eleventh", image: "img/eleventh.jpeg" },
 ];
 
-//Initial time
+// Initial time
 let seconds = 0;
 let minutes = 0;
 
-//Initial moves and win count
+// Initial moves and win count
 let movesCount = 0;
 let winCount = 0;
 
-//Timer
+// Timer
 const timeGenerator = () => {
   seconds += 1;
   if (seconds >= 60) {
@@ -48,25 +49,25 @@ const timeGenerator = () => {
   timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 };
 
-//Calculating moves
+// Calculating moves
 const movesCounter = () => {
   movesCount += 1;
   moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
-//Pick random objects from the items array
+// Pick random objects from the items array
 const generateRandom = (size = 4) => {
-  //tempo array
+  // temp array
   let tempArray = [...items];
-  //initializes cardValues array
+  // initializes cardValues array
   let cardValues = [];
-  //size (4*4)
+  // size (4*4)
   size = (size * size) / 2;
-  //random object selection 
+  // random object selection
   for (let i = 0; i < size; i++) {
     const randomIndex = Math.floor(Math.random() * tempArray.length);
     cardValues.push(tempArray[randomIndex]);
-    //once selected remove the object from temp array
+    // once selected, remove the object from temp array
     tempArray.splice(randomIndex, 1);
   }
   return cardValues;
@@ -75,24 +76,27 @@ const generateRandom = (size = 4) => {
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
-  //simple shuffle
-  cardValues.sort(() => Math.random() - 0,5);
-  for (let i =0; i < size*size; i++) {
+  // simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
     gameContainer.innerHTML += `
-    <div class = "card-container" data-card-value = "${cardValues[i].name}">
-     <div class = "card-before">?</div>
-     <div class = "card-after">
-     <img src = "${cardValues[i].image}" class = "image"/></div>
-     </div>
+      <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before">?</div>
+        <div class="card-after">
+          <img src="${cardValues[i].image}" class="image" />
+        </div>
+      </div>
     `;
   }
-};
-
-//Initialize values and func calls
+  
+// Initialize values and func calls
 const initializer = () => {
   result.innerText = "";
   winCount = 0;
   let cardValues = generateRandom();
   matrixGenerator(cardValues);
 };
+
+initializer();
+
 
